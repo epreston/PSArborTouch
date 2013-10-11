59af5126d750e526622f62dce4d284d7494dbdf8
Ed Preston <epreston@eds-macbook-pro.gateway.2wire.net>
2013-10-11 19:44:32 -0400

    Modernised System Rig’s main function.

--- TestRigs/SystemRig/main.m
+++ TestRigs/SystemRig/main.m
@@ -8,12 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
+#import "SystemRigAppDelegate.h"
+
 int main(int argc, char *argv[])
 {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    int retVal = UIApplicationMain(argc, argv, nil, nil);
-    [pool release];
-    return retVal;
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SystemRigAppDelegate class]));
+    }
 }
 
 
